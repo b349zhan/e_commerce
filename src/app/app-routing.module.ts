06da1660471f0ff6c8f26f9ad5fec3c2d12c8411
Bowen Zhang <b349zhan@Bowens-MacBook-Air.local>
2022-03-19 18:01:05 -0400

    added admin product management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'products', component: ProductComponent},
+  {path: 'cart', component: CartComponent },
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'orders', component:MyOrderComponent},
+  {path: 'order-success', component: OrderSuccessComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'admin/products/new', component: ProductFormComponent},
+  {path: 'admin/products/:id', component: ProductFormComponent},
+  {path: 'admin/products', component: AdminProductsComponent},
+  {path: 'admin/orders', component: AdminOrdersComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { ProductComponent } from './product/product.component';
+import { LoginComponent } from './login/login.component';
+import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CartComponent,
+    CheckoutComponent,
+    OrderSuccessComponent,
+    MyOrderComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    ProductComponent,
+    LoginComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
